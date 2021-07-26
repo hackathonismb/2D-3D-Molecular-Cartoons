6bc116f370f20d16f21f67d0775213301c03af6a
kyang127 <87899337+kyang127@users.noreply.github.com>
2021-07-26 10:34:21 -0400

    Renamed to CartoonCenterTester.java

--- CartoonCenterTester.java
+++ CartoonCenterTester.java
@@ -1,18 +1,18 @@
-public class 2DCartoonCenterTester {
+public class CartoonCenterTester {
     public static void main(String[] args){
         XYZPoint[] pointList;
         Center finder = new Center();
 
-        Center.XYZPoint point1 = finder.new XYZPoint(1.0,1.0,1.0);
-        Center.XYZPoint point2 = finder.new XYZPoint(2.0,2.0,2.0);
-        Center.XYZPoint point3 = finder.new XYZPoint(3.0,3.0,3.0);
-        Center.XYZPoint point4 = finder.new XYZPoint(4.0,4.0,4.0);
-        Center.XYZPoint point5 = finder.new XYZPoint(5.0,5.0,5.0);
-        Center.XYZPoint point6 = finder.new XYZPoint(6.0,6.0,4.0);
-        Center.XYZPoint point7 = finder.new XYZPoint(7.0,7.0,3.0);
-        Center.XYZPoint point8 = finder.new XYZPoint(8.0,8.0,2.0);
-        Center.XYZPoint point9 = finder.new XYZPoint(9.0,9.0,1.0);
-        Center.XYZPoint point10 = finder.new XYZPoint(10.0,10.0,2.5);
+        CartoonCenter.XYZPoint point1 = finder.new XYZPoint(1.0,1.0,1.0);
+        CartoonCenter.XYZPoint point2 = finder.new XYZPoint(2.0,2.0,2.0);
+        CartoonCenter.XYZPoint point3 = finder.new XYZPoint(3.0,3.0,3.0);
+        CartoonCenter.XYZPoint point4 = finder.new XYZPoint(4.0,4.0,4.0);
+        CartoonCenter.XYZPoint point5 = finder.new XYZPoint(5.0,5.0,5.0);
+        CartoonCenter.XYZPoint point6 = finder.new XYZPoint(6.0,6.0,4.0);
+        CartoonCenter.XYZPoint point7 = finder.new XYZPoint(7.0,7.0,3.0);
+        CartoonCenter.XYZPoint point8 = finder.new XYZPoint(8.0,8.0,2.0);
+        CartoonCenter.XYZPoint point9 = finder.new XYZPoint(9.0,9.0,1.0);
+        CartoonCenter.XYZPoint point10 = finder.new XYZPoint(10.0,10.0,2.5);
         
         pointList = new XYZPoint[]{point1,point2,point3,point4,point5,point6,point7,point8,point9,point10};
 
